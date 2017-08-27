289a28f1db98035a0f63efdccce407b56595d437
Albert Pak <al@albert-pak.com>
2017-08-27 14:00:30 -0500

    Starting to build and style music api app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" placeholder="search an artist..." />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search" />
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">Gallery</div>
+      </div>
+    );
+  }
+}
+
+export default App;
