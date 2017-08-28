ce210be0735aafd851f5bd4e82bbc571210f3e51
Albert Pak <al@albert-pak.com>
2017-08-27 23:19:39 -0500

    wip - music api

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,44 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+    };
+  }
+
+  search() {
+    console.log('this.state', this.state);
+
+    const BASE_SEARCH_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = `${BASE_SEARCH_URL}q=${this.state
+      .query}&type=artist&limit=1`;
+
+    console.log('FETCH_URL', FETCH_URL);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-title">Music Master</div>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="search an artist..." />
-            <InputGroup.Addon>
+            <FormControl
+              type="text"
+              placeholder="search an artist..."
+              value={this.state.query}
+              onChange={event => {
+                this.setState({ query: event.target.value });
+              }}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search" />
             </InputGroup.Addon>
           </InputGroup>
